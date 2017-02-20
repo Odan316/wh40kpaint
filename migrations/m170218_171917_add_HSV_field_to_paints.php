72f7a3431fb900ed1bd7b6cc6728b9a44bd2f4f0
Andreev Sergey <si.andreev@gmail.com>
2017-02-20 11:51:01 +0300

    HSV p1.1

--- migrations/m170218_171917_add_HSV_field_to_paints.php
+++ migrations/m170218_171917_add_HSV_field_to_paints.php
@@ -2,18 +2,26 @@
 
 use yii\db\Migration;
 
-class m170218_171917_add_HSV_field_to_paints extends Migration
+class m170218_171917_add_RGB_HSV_HSL_field_to_paints extends Migration
 {
     public function up()
     {
-        $this->addColumn('paint', 'hsv_h', $this->float(12));
-        $this->addColumn('paint', 'hsv_s', $this->float(12));
-        $this->addColumn('paint', 'hsv_v', $this->float(12));
+        $this->addColumn('paint', 'rgb_r', $this->smallInteger());
+        $this->addColumn('paint', 'rgb_g', $this->smallInteger());
+        $this->addColumn('paint', 'rgb_b', $this->smallInteger());
+
+        $this->addColumn('paint', 'hsv_h', $this->smallInteger());
+        $this->addColumn('paint', 'hsv_s', $this->smallInteger());
+        $this->addColumn('paint', 'hsv_v', $this->smallInteger());
+
+        $this->addColumn('paint', 'hsl_h', $this->smallInteger());
+        $this->addColumn('paint', 'hsl_s', $this->smallInteger());
+        $this->addColumn('paint', 'hsl_l', $this->smallInteger());
     }
 
     public function down()
     {
-        echo "m170218_171917_add_HSV_field_to_paints cannot be reverted.\n";
+        echo "m170218_171917_add_RGB_HSV_HSL_field_to_paints cannot be reverted.\n";
 
         return false;
     }
